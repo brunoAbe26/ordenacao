625824147b0de3e16cd94615a8e9e7ecdf8c0bd0
bruno abe <darksorcerer43@gmail.com>
2018-09-01 15:24:32 -0300

    implementando o heap sort

--- funcoes_auxiliares.py
+++ funcoes_auxiliares.py
@@ -1,6 +1,11 @@
+#-*- coding: UTF-8 -*-
+
 from random import sample, choice
 from time import time
+
+
 def troca(v, i, j): v[i], v[j] = v[j], v[i]
+
 
 def seleciona(v, i):
     m = 0
@@ -8,11 +13,29 @@ def seleciona(v, i):
         if v[m]<v[j]: m = j
     return m
 
+
 def insere(x, v, u):
     while u>=0 and x<v[u]:
         v[u+1] = v[u]
         u -= 1
     v[u+1] = x
+
+
+def desce(v, i, n):
+    while i*2 < n:
+        j = i*2
+        if j+1 < n and v[j] < v[j+1]: j += 1
+        if v[i] >= v[j]: break
+        troca(v, i, j)
+        i = j
+
+
+def amontoa(v):
+    n = len(v)
+    for i in reversed(range(n//2)):
+        desce(v, i, n)
+
+
 
 def seq(n, *args):
     if len(args) == 0: return sample(list(range(n*10)), n)
@@ -21,11 +44,13 @@ def seq(n, *args):
         return [choice(v) for x in range(n)]
     else: raise Exception('Você digitou os parâmetros errados')
 
+
 def crescente(v):
     n = len(v)
     for i in range(n-1):
         if v[i]>v[i+1]: return False
     return True
+
 
 def tempo(f, v):
     t_inicial = time()
--- ordenacao.py
+++ ordenacao.py
@@ -1,4 +1,5 @@
-from funcoes_auxiliares import troca, seq, seleciona, insere
+#-*- coding: UTF-8 -*-
+from funcoes_auxiliares import troca, seq, seleciona, insere, amontoa, desce
 
 def bsort(v):
     n = len(v)
@@ -6,13 +7,23 @@ def bsort(v):
         for j in range(1,n-i):
             if v[j-1]>v[j]: troca(v, j-1, j)
 
+
 def ssort(v):
     n = len(v)
     for i in reversed(range(1, n)):
         j = seleciona(v, i+1)
         troca(v,i,j)
 
+
 def isort(v):
     n = len(v)
     for i in range(1, n):
         insere(v[i], v, i-1)
+
+
+def hsort(v):
+    n = len(v)
+    amontoa(v)
+    for i in reversed(range(n)):
+        troca(v, 0, i)
+        desce(v, 0, i)
